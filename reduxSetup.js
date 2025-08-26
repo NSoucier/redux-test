3c05b646ae3715f7949dc0a35a506acad28ac37e
Nathan Soucier <soucienp@gmail.com>
2025-08-26 11:16:58 -0400

    creating basic actions and types in a count reducer

---
+++ reduxSetup.js
@@ -0,0 +1,12 @@
+const INITIAL_STATE = { count: 0}
+
+const countReducer = (state = INITIAL_STATE, action) => {
+    if(action.type === 'LOG_STATE') {
+        console.log('HERE IS YOUR STATE: ', state)
+        return state;
+    }
+    return state;
+}
+
+const store = Redux.createStore(countReducer);
+store.dispatch({ type: 'LOG_STATE' });
